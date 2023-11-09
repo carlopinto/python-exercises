8bb8d23327770eea9e260f31bd0f03498846457e
Carlo Pinto <carlo.pinto@calytrix.com>
2023-11-09 15:09:58 +0000

    Add more list algorithms to vector_methods [linear and binary search]

--- Vectors/test_vectors_methods.py
+++ Vectors/test_vectors_methods.py
@@ -77,6 +77,99 @@ class TestVectorsMethods(unittest.TestCase):
         self.assertNotEqual(b, ["This", "is", "fun"])
         self.assertNotEqual(a, [2,3,4])
 
+    def test_search_linear(self):
+        """"""
+        sz = 10000000 # Lets have 10 million elements in the list
+        xs = range(sz)
+        self.assertEqual(vectors_methods.search_linear(xs, 9999999), 9999999)
+        self.assertEqual(vectors_methods.search_linear(xs, 99999999), -1)
+
+    def test_find_unknown_words(self):
+        """"""
+        vocab = ["apple", "boy", "dog", "down", 
+                          "fell", "girl", "grass", "the", "tree"]
+        book_words = "the apple fell from the tree to the grass".split()
+        self.assertEqual(vectors_methods.find_unknown_words(vocab, book_words), ["from", "to"])
+        self.assertEqual(vectors_methods.find_unknown_words([], book_words), book_words)
+        self.assertEqual(vectors_methods.find_unknown_words(vocab, ["the", "boy", "fell"]), [])
+
+    def test_text_to_words(self):
+        """"""
+        words = "the apple fell from the tree to the grass"
+        self.assertEqual(vectors_methods.text_to_words(words), ["the", "apple", "fell", "from", "the", "tree", "to", "the", "grass"])
+
+        self.assertEqual(vectors_methods.text_to_words(""), [])
+        self.assertEqual(vectors_methods.text_to_words(" "), [])
+        self.assertEqual(vectors_methods.text_to_words("hello"), ["hello"])
+
+    def test_get_words_in_book(self):
+        """"""
+        book_words = vectors_methods.get_words_in_book("Vectors/alice_in_wonderland.txt")
+        self.assertEqual(len(book_words), 27336)
+
+    def test_load_words_from_file(self):
+        """"""
+        book_words = vectors_methods.load_words_from_file("Vectors/alice_in_wonderland.txt")
+        self.assertEqual(len(book_words), 26443)
+        
+    def test_search_binary(self):
+        """"""
+        sz = 10000000 # Lets have 10 million elements in the list
+        xs = range(sz)
+        self.assertEqual(vectors_methods.search_linear(xs, 9999999), 9999999)
+        self.assertEqual(vectors_methods.search_linear(xs, 99999999), -1)
+
+    def test_remove_adjacent_dups(self):
+        """"""
+        all_words = vectors_methods.get_words_in_book("Vectors/alice_in_wonderland.txt")
+        all_words.sort()
+        book_words = vectors_methods.remove_adjacent_dups(all_words)
+        # All words
+        self.assertEqual(len(all_words), 27336)
+        # Unique words
+        self.assertEqual(len(book_words), 2569)
+
+    def test_merge(self):
+        """"""
+        xs = [1,3,5,7,9,11,13,15,17,19,20,24]
+        ys = [3,4,8,12,16,20,24]
+        self.assertEqual(vectors_methods.merge(xs, ys), [1, 3, 3, 4, 5, 7, 8, 9, 11, 12, 13, 15, 16, 17, 19, 20, 20, 24, 24])
+
+    def test_merge_v1(self):
+        """"""
+        xs = [1,3,5,7,9,11,13,15,17,19,20,24]
+        ys = [3,4,8,12,16,20,24]
+        self.assertEqual(vectors_methods.merge_v1(xs, ys), [3, 20, 24])
+
+    def test_merge_v2(self):
+        """"""
+        xs = [1,3,5,7,9,11,13,15,17,19,20,24]
+        ys = [3,4,8,12,16,20,24]
+        self.assertEqual(vectors_methods.merge_v2(xs, ys), [1, 5, 7, 9, 11, 13, 15, 17, 19])
+
+    def test_find_unknowns_merge_pattern(self):
+        """"""
+        xs = [1,3,5,7,9,11,13,15,17,19,20,24]
+        ys = [3,4,8,12,16,20,24]
+        self.assertEqual(vectors_methods.find_unknowns_merge_pattern(xs, ys), [4, 8, 12, 16])
+
+    def test_merge_v4(self):
+        """"""
+        xs = [1,3,5,7,9,11,13,15,17,19,20,24]
+        ys = [3,4,8,12,16,20,24]
+        self.assertEqual(vectors_methods.merge_v4(xs, ys), [1, 3, 4, 5, 7, 8, 9, 11, 12, 13, 15, 16, 17, 19, 20, 24])
+
+    def test_merge_v5(self):
+        """ an item in the second list “knocks out” just one matching item in the first list """
+        
+        xs5 = [5,7,11,11,11,12,13]
+        ys5 = [7,8,11]
+        self.assertEqual(vectors_methods.merge_v5(xs5, ys5), [5, 11, 11, 12, 13])
+
+        xs = [1,3,5,7,9,11,13,15,17,19,20,24]
+        ys = [3,4,8,12,16,20,24]
+        self.assertEqual(vectors_methods.merge_v5(xs, ys), [1, 5, 7, 9, 11, 13, 15, 17, 19])
+
 
 if __name__ == '__main__':
     unittest.main()
--- Vectors/vectors_methods.py
+++ Vectors/vectors_methods.py
@@ -47,3 +47,256 @@ def swap(x, y):
     (x, y) = (y, x)
     # print("after swap statement: x:", x, "y:", y)
     return (x, y)
+
+def search_linear(xs, target):
+    """ Find and return the index of target in sequence xs """
+    for (i, v) in enumerate(xs):
+        if v == target:
+            return i
+    return -1
+
+def find_unknown_words(vocab, wds):
+    """ Return a list of words in wds that do not occur in vocab """
+    result = []
+    for w in wds:
+        if (search_binary(vocab, w) < 0):
+            result.append(w)
+    return result
+
+def text_to_words(the_text):
+    """ return a list of words with all punctuation removed,
+    and all in lowercase.
+    """
+
+    my_substitutions = the_text.maketrans(
+        # If you find any of these
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&()*+,-./:;<=>?@[]^_`{|}~'\\",
+        # Replace them by these
+        "abcdefghijklmnopqrstuvwxyz                                          ")
+
+    # Translate the text now.
+    cleaned_text = the_text.translate(my_substitutions)
+    wds = cleaned_text.split()
+    return wds
+
+def get_words_in_book(filename):
+    """ Read a book from filename, and return a list of its words. """
+    f = open(filename, "r")
+    content = f.read()
+    f.close()
+    wds = text_to_words(content)
+    return wds
+
+def load_words_from_file(filename):
+    """ Read words from filename, return list of words. """
+    f = open(filename, "r")
+    file_content = f.read()
+    f.close()
+    wds = file_content.split()
+    return wds
+
+### The word binary means two. Binary search gets its name from the fact that each probe splits the list
+### into two pieces and discards the one half from the region of interest.
+def search_binary(xs, target):
+    """ Find and return the index of key in sequence xs """
+    lb = 0          # lower bound
+    ub = len(xs)    # upper bound
+    while True:
+        if lb == ub: # If region of interest (ROI) becomes empty
+            return -1
+
+        # Next probe should be in the middle of the ROI
+        mid_index = (lb + ub) // 2
+
+        # Fetch the item at that position
+        item_at_mid = xs[mid_index]
+
+        # print("ROI[{0}:{1}](size={2}), probed='{3}', target='{4}'"
+        #  .format(lb, ub, ub-lb, item_at_mid, target))
+
+        # How does the probed item compare to the target?
+        if item_at_mid == target:
+            return mid_index # Found it!
+        if item_at_mid < target:
+            lb = mid_index + 1 # Use upper half of ROI next time
+        else:
+            ub = mid_index # Use lower half of ROI next time
+
+def remove_adjacent_dups(xs):
+    """ Return a new list in which all adjacent
+    duplicates from xs have been removed.
+    """
+    result = []
+    most_recent_elem = None
+    for e in xs:
+        if e != most_recent_elem:
+            result.append(e)
+            most_recent_elem = e
+
+    return result
+
+def merge(xs, ys):
+    """ merge sorted lists xs and ys. Return a sorted result """
+    result = []
+    xi = 0
+    yi = 0
+
+    while True:
+        if xi >= len(xs): # If xs list is finished,
+            result.extend(ys[yi:]) # Add remaining items from ys
+            return result # And we're done.
+
+        if yi >= len(ys): # Same again, but swap roles
+            result.extend(xs[xi:])
+            return result
+
+        # Both lists still have items, copy smaller item to result.
+        if xs[xi] <= ys[yi]:
+            result.append(xs[xi])
+            xi += 1
+        else:
+            result.append(ys[yi])
+            yi += 1
+
+def merge_v1(xs, ys):
+    """ merge sorted lists xs and ys. Return a sorted result 
+        Return only those items that are present in both lists.
+    """
+    result = []
+    xi = 0
+    yi = 0
+
+    while True:
+        if xi >= len(xs):
+            return result
+
+        if yi >= len(ys):
+            return result
+
+        if xs[xi] == ys[yi]: # Good, present in both lists
+            result.append(ys[yi])
+            xi += 1
+            yi += 1
+
+        elif xs[xi] < ys[yi]: # Move past this vocab word,
+            xi += 1
+
+        else: # Got word that is not in vocab
+            yi += 1
+
+def merge_v2(xs, ys):
+    """ merge sorted lists xs and ys. Return a sorted result 
+        Return only those items that are present in the first list, but not in the second.
+    """
+    result = []
+    xi = 0
+    yi = 0
+
+    while True:
+        if xi >= len(xs):
+            return result
+
+        if yi >= len(ys):
+            return result
+
+        if xs[xi] == ys[yi]: # move on
+            xi += 1
+            yi += 1
+
+        elif xs[xi] < ys[yi]: # Good, present in first list in second
+            result.append(xs[xi])
+            xi += 1
+
+        else: # move on in second list
+            yi += 1
+
+# or merge_v3
+def find_unknowns_merge_pattern(vocab, wds):
+    """ Both the vocab and wds must be sorted. Return a new
+    list of words from wds that do not occur in vocab.
+    Return only those items that are present in the second list, but not in the first.
+    """
+    result = []
+    xi = 0
+    yi = 0
+
+    while True:
+        if xi >= len(vocab):
+            result.extend(wds[yi:])
+            return result
+
+        if yi >= len(wds):
+            return result
+
+        if vocab[xi] == wds[yi]: # Good, word exists in vocab
+            yi += 1
+
+        elif vocab[xi] < wds[yi]: # Move past this vocab word,
+            xi += 1
+
+        else: # Got word that is not in vocab
+            result.append(wds[yi])
+            yi += 1
+
+def merge_v4(xs, ys):
+    """ merge sorted lists xs and ys. Return a sorted result 
+        Return items that are present in either the first or the second list.
+    """
+    result = []
+    xi = 0
+    yi = 0
+    previous = None
+
+    while True:
+        if xi >= len(xs):
+            return result
+
+        if yi >= len(ys):
+            return result
+        
+        if  xs[xi] == previous:
+            xi +=1
+
+        if  ys[yi] == previous:
+            yi +=1
+
+        if xs[xi] == ys[yi]: # move on
+            result.append(xs[xi])
+            previous = xs[xi]
+            xi += 1
+            yi += 1
+
+        elif xs[xi] < ys[yi]: # Good, present in first list in second
+            result.append(xs[xi])
+            previous = xs[xi]
+            xi += 1
+
+        else: # move on in second list
+            result.append(ys[yi])
+            previous = ys[yi]
+            yi += 1
+
+def merge_v5(xs, ys):
+    """ merge sorted lists xs and ys. Return a sorted result 
+        Return items from the first list that are not eliminated 
+        by a matching element in the second list.
+    """
+    result = []
+    xi = 0
+    yi = 0
+
+    while(True):
+        if yi >= len(ys):
+            if len(xs) > 0 and xi < len(xs):
+                result.append(xs[xi])
+                yi = 0
+                xi += 1
+        
+        if xi >= len(xs):
+            return result
+        
+        if xs[xi] == ys[yi]:
+            ys = ys[:yi] + ys[yi+1:]
+            xi += 1
+        else:
+            yi += 1
